1c120df1d28126edeaf5dc1f656d13ea9cc7a970
TinyKitten <catwalk8205@gmail.com>
2020-01-05 09:19:13 +0900

    ServiceWorkerアップデート

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,47 +1,78 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, async } from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {AlertController, Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SwUpdate} from '@angular/service-worker';
+import {Subject} from 'rxjs';
+
+class MockSwUpdate {
+  $$availableSubj = new Subject<{ available: { hash: string } }>();
+  $$activatedSubj = new Subject<{ current: { hash: string } }>();
+
+  available = this.$$availableSubj.asObservable();
+  activated = this.$$activatedSubj.asObservable();
+
+  activateUpdate = jasmine.createSpy('MockSwUpdate.activateUpdate')
+    .and.callFake(() => Promise.resolve());
+
+  checkForUpdate = jasmine.createSpy('MockSwUpdate.checkForUpdate')
+    .and.callFake(() => Promise.resolve());
+
+  constructor(public isEnabled: boolean) {
+  }
+}
 
 describe('AppComponent', () => {
 
   let statusBarSpy, splashScreenSpy, platformReadySpy, platformSpy;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let swu: MockSwUpdate;
+  const alertCtrlSpy = jasmine.createSpyObj('AlertController', ['create']);
 
   beforeEach(async(() => {
     statusBarSpy = jasmine.createSpyObj('StatusBar', ['styleDefault']);
     splashScreenSpy = jasmine.createSpyObj('SplashScreen', ['hide']);
     platformReadySpy = Promise.resolve();
-    platformSpy = jasmine.createSpyObj('Platform', { ready: platformReadySpy });
+    platformSpy = jasmine.createSpyObj('Platform', {ready: platformReadySpy});
+    swu = new MockSwUpdate(true);
 
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        { provide: StatusBar, useValue: statusBarSpy },
-        { provide: SplashScreen, useValue: splashScreenSpy },
-        { provide: Platform, useValue: platformSpy },
+        {provide: StatusBar, useValue: statusBarSpy},
+        {provide: SplashScreen, useValue: splashScreenSpy},
+        {provide: Platform, useValue: platformSpy},
+        {provide: SwUpdate, useValue: swu},
+        {provide: AlertController, useValue: alertCtrlSpy}
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should initialize the app', async () => {
-    TestBed.createComponent(AppComponent);
     expect(platformSpy.ready).toHaveBeenCalled();
     await platformReadySpy;
     expect(statusBarSpy.styleDefault).toHaveBeenCalled();
     expect(splashScreenSpy.hide).toHaveBeenCalled();
+    spyOn(app as any, 'promptUpdate').and.callThrough();
+    swu.$$availableSubj.next({available: {hash: 'foo'}});
+    expect((app as any).promptUpdate).toHaveBeenCalled();
   });
 
-  // TODO: add more tests!
-
+  it('promptUpdate', () => {
+    (app as any).promptUpdate();
+    expect(alertCtrlSpy.create).toHaveBeenCalled();
+  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {AlertController, Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private alertCtrl: AlertController,
+    private updates: SwUpdate
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,25 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.updates.available.subscribe(() => {
+        this.promptUpdate();
+      });
     });
+  }
+
+  private async promptUpdate() {
+    const alert = await this.alertCtrl.create({
+      header: 'ポータルのアップデートがあります。今すぐ適用しますか？',
+      buttons: [
+        {
+          text: 'はい',
+          handler: () => {
+            this.updates.activateUpdate().then(() => document.location.reload());
+          }
+        },
+        'いいえ'
+      ]
+    });
+    alert.present();
   }
 }
